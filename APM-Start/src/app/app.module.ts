76ec3b746386cf16ec2ccdea852683acb0262c02
Lazyspell <Jeremy2975@yahoo.com>
2020-08-18 10:10:26 -0700

    adding stuff

---
+++ APM-Start/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms'
+
+import {ProductListComponent} from './products/product-list.component'
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
